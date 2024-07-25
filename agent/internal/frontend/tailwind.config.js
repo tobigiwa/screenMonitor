5af5e55278e6ce5857b846f05fa7091db6aa1b54
tobigiwa <giwaoluwatobi@gmail.com>
2024-07-25 10:52:18 +0100

    screentime page about 85% tailwindcss
    Signed-off-by: tobigiwa <giwaoluwatobi@gmail.com>

--- agent/internal/frontend/tailwind.config.js
+++ agent/internal/frontend/tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./components/*.templ"],
   theme: {
-    extend: {},
+    screens: {
+      scTwo: {'max':'700px'},
+      scTextOne: {'max':'620px'},
+      scTextTwo: {'max':'555px'},
+      scOne: {'max':'550px'},
+    },
+    extend: {
+      spacing: {
+        '99%': '99%',
+        'chart-highlight': 'calc(100vh - 37rem)',
+      }
+    },
   },
   plugins: [],
 }
